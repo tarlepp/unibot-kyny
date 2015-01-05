a4932d20f51bb6eeee2d694394fa7d4dccce26e7
tarlepp <tarmo.leppanen@kase.fi>
2015-01-05 17:24:05 +0200

    Refactored plugin.

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ var cheerio = require('cheerio');
  * Also note that this plugin relies heavily to HTML structure of that site. So there will be times, when this plugin
  * doesn't work right.
  *
+ * @todo    Make shown event count configurable
+ * @todo    Make bot say founded events in one message (need more opinions about this)
+ *
  * @param   {Object} options Plugin options object, description below.
  *   db: {mongoose} the mongodb connection
  *   bot: {irc} the irc bot
@@ -29,15 +32,29 @@ module.exports = function init(options) {
         return {
             '^!kyny$': function kyny(from, matches) {
                 helpers.download('http://www.mustakynnys.com/', function success(data) {
-                    var $ = cheerio.load(data);
+                    if (data == null) {
+                        channel.say(from, 'Did not get any data from http://www.mustakynnys.com/ - Site down?');
+                    } else {
+                        var $ = cheerio.load(data);
+                        var found = false;
 
-                    $('#boxleft p.pvm').each(function iterator(i, elem) {
-                        if (i < 3) {
-                            var event = $(this);
+                        $('#boxleft').find('p.pvm').each(function iterator(i, elem) {
+                            if (i < 3) {
+                                var event = $(this);
+                                var text = event.text().trim() + " " + event.next().text().split('\n')[1];
 
-                            channel.say(event.text().trim() + " " + event.next().text().split('\n')[1]);
+                                if (text) {
+                                    found = true;
+
+                                    channel.say(text);
+                                }
+                            }
+                        });
+
+                        if (!found) {
+                            channel.say(from, 'Did not found any event data...');
                         }
-                    });
+                    }
                 });
             }
         };
